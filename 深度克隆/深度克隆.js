86cd8002f4fb7ad57009ad9b00208c26be98748c
jason <xygjsj@139.com>
2016-03-03 19:42:56 +0800

    深度克隆

---
+++ 深度克隆/深度克隆.js
@@ -0,0 +1,32 @@
+function getClass(obj){//得到对象类型
+  return Object.prototype.toString.call(obj);
+}
+function cloneObject(src){
+  var result = getClass(src) === Array ? []:{};//为【for-in】循环做准备
+    for(var key in src){
+      if(typeof src[key] === 'object' && src[key] !== null){
+        result[key] = arguments.callee(src[key]);
+      }
+      else{
+        result[key] = src[key];
+      }
+    }
+    return result;
+}
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+// 例子
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+console.log(abObj.a);        //2
+console.log(abObj.b.b1[0]);  //Hello
+console.log(tarObj.a);       //1
+console.log(tarObj.b.b1[0]); //hello
