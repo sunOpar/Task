d2cecf6543bc351a9c1244aa67289a739cfb6893
xygjsj@139.com <xygjsj@139.com>
2016-06-25 13:35:37 +0800

    add circle fadein and fadeout

---
+++ 循环动画淡入淡出/task.js
@@ -0,0 +1,31 @@
+function Banner(wrap,li,showTime,hideTime){
+	this.wrap = wrap;
+	this.li = li;
+	this.showTime = showTime;
+	this.hideTime = hideTime;
+	this.DELY = hideTime - 1500;
+}
+Banner.prototype.init = function(){
+	// setInterval(function(){
+	var that = this; 
+	that.cyclePlay(that,this.li.length);
+	setInterval(function(){
+		that.cyclePlay(that,that.li.length);
+	},that.hideTime*that.li.length);
+}
+Banner.prototype.cyclePlay = function(that,length){
+	var oldRight = '';
+	for(var i = 0; i < length;i++){
+		$(that.li[i]).children().delay(that.DELY*i).animate({
+		opacity: '1'
+	},that.showTime);
+			oldRight = $(that.li[i]).children().css('right');
+			$(that.li[i]).children().animate({
+			right: '450px',
+			opacity: '0'
+		},that.hideTime);
+			$(that.li[i]).children().animate({
+				right: oldRight,
+			},1);
+	}
+}
